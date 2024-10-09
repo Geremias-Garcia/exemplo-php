28feb276c46410a0a80e7b187efe0b34aed16e7b
Geremias Garcia <88598645+Geremias-Garcia@users.noreply.github.com>
2024-10-09 08:51:41 -0300

    Add files via upload

---
+++ des-web-III/deletaCliente.php
@@ -0,0 +1,19 @@
+<?php
+    require 'Banco.php';
+    require 'Cliente.php';
+
+    $database = new Banco();
+    $db = $database->getConexao();
+
+    $cliente = new Cliente($db);
+
+    $cliente->setId($_GET['id']);
+
+    if ($cliente->delete()) {
+        echo "Cliente deletado com sucesso!";
+        header("Refresh:3;url=listarCliente.php");
+    } else {
+        echo "Erro ao deletar o cliente.";
+        header("Refresh:3;url=listarCliente.php");
+    }
+?>
